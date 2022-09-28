6c7e18e65293a7859bb8172e5e24271ae406ebad
Daniel Garwood <daniel.garwood@bbc.co.uk>
2022-09-28 15:59:12 +0100

    added search for List All

---
+++ index.js
@@ -0,0 +1,8 @@
+const readline = require('readline-sync');
+
+console.log('Please enter some input:');
+const response = readline.prompt();
+
+if (response == "List All") {
+    console.log(response);
+}
